425254f63c8f658b399619469751f35cb61ef4d1
Barkın ZORLU <barkinzorlu@outlook.com>
2024-10-16 22:48:44 +0300

    Add Raycaster

---
+++ Scripts/Camera/Raycaster.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public partial class Raycaster : Camera3D // random name idk
+{
+
+    [Export] private float rayLength = 300;
+
+    public override void _Input(InputEvent @event)
+    {
+
+        SelectViaRaycast();
+        base._Input(@event);
+    }
+
+    private void SelectViaRaycast()
+    {
+        if (Input.IsActionJustPressed("Select"))
+        {
+            //var from = CameraManager.Instance.mainCamera.ProjectRayOrigin(GetViewport().GetMousePosition());
+			//var to = from + CameraManager.Instance.mainCamera.ProjectRayNormal(GetViewport().GetMousePosition()) * RayLength;
+
+            var from = this.ProjectRayOrigin(GetViewport().GetMousePosition());
+            var to = from + ProjectRayNormal(GetViewport().GetMousePosition()) * rayLength;
+
+            CastHit hit = PhysicsCasts.CastLine(this, from, to, PhysicsCasts.GetCollisionMask(1) ,true);
+
+            if(hit.NonEmpty)
+            {
+                GD.Print(hit.Collider.GetParent().Name);
+            }
+            else
+                GD.Print("No Hit");
+        }
+    }
+
+}
