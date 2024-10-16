f29c8b972e383d0b76ad74dd85d8982771798362
Barkın ZORLU <barkinzorlu@outlook.com>
2024-10-16 22:12:48 +0300

    Add GridManager, grid

---
+++ Scripts/Managers/GridManager.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+public partial class GridManager : Node
+{
+
+    public static GridManager Instance {get; private set;}
+    [Export] public Resource gridData; // TODO: change to grid data resources type
+
+    [Export] public PackedScene gridObjectSubscene;
+    [Export] public Node3D gridParent;
+
+    [Export] private int gridSize; // temp, will check from data
+
+    [Export] public Godot.Collections.Array<Godot.Collections.Array<Node3D>> grids = new();
+
+    private GridManager()
+    {
+        Instance = this;
+    }
+
+    public override void _Ready()
+    {
+        CreateGrid();
+        base._Ready();
+    }
+
+    private void CreateGrid()
+    {
+        for(int i = 0; i < gridSize; i++)
+        {
+            Godot.Collections.Array<Node3D> innerArray = new();
+            for (int j = 0; j < gridSize; j++)
+            {
+                var gridObject = gridObjectSubscene.Instantiate<Node3D>();
+                gridObject.Position = new Vector3(i, 0, j);
+                gridParent.AddChild(gridObject);
+                innerArray.Add(gridObject);
+            }
+            grids.Add(innerArray);
+        }
+    }
+}
