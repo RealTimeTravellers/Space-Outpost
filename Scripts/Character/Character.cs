f99db6b6479f431a0008d3ae4c7b5c9219612a80
Barkın ZORLU <barkinzorlu@outlook.com>
2024-10-17 22:47:31 +0300

    Fix character extention

--- Scripts/Character/Character.cs
+++ Scripts/Character/Character.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Character : Node3D
+public partial class Character : CharacterBody3D // don't really know why is this character body
 {
     public GridObject currentGrid = null;
 }
