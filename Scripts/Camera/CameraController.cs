419ce2c3a7d32389d6abb1b5abb6c804e0bfac9c
Barkın ZORLU <barkinzorlu@outlook.com>
2024-12-02 14:43:35 +0300

    Update CameraController

--- Scripts/Camera/CameraController.cs
+++ Scripts/Camera/CameraController.cs
@@ -24,6 +24,8 @@ public partial class CameraController : Node3D
     {
         // TODO: Tween?
 
+        if (CameraManager.Instance.AimingMode) return;
+
         if(Input.IsActionPressed("Move Left"))
             this.Position += cameraSpeed * Vector3.Left * delta;
         else if (Input.IsActionPressed("Move Right"))
@@ -33,13 +35,21 @@ public partial class CameraController : Node3D
             this.Position += cameraSpeed * Vector3.Forward * delta;
         else if (Input.IsActionPressed("Move Backward"))
             this.Position += cameraSpeed * Vector3.Back * delta;
+        
+        if (!CameraManager.Instance.AimingMode)
+            CameraManager.Instance.TacticalCameraPostion.Position = Position;
     }
 
     private void TacticalCameraZoom(InputEvent @event)
     {
+        if (CameraManager.Instance.AimingMode) return; // IDEA: maybe switch to fov zoom
+
         if(@event.IsAction("Zoom In"))
             Position += zoomSpeed * Vector3.Up;
         else if (@event.IsAction("Zoom Out"))
             Position += zoomSpeed * Vector3.Down;
+        
+        if (!CameraManager.Instance.AimingMode)
+            CameraManager.Instance.TacticalCameraPostion.Position = Position;
     }
 }
