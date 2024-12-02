5d3fdfeb387bb24c5b2278f5da06f5654bec5a9c
Barkın ZORLU <barkinzorlu@outlook.com>
2024-12-02 14:35:42 +0300

    Added Delta and seperated speeds V/H

--- Scripts/Camera/CameraController.cs
+++ Scripts/Camera/CameraController.cs
@@ -5,11 +5,12 @@ public partial class CameraController : Node3D
 {
     [Export] Raycaster raycaster;
     [Export] public Camera3D Camera {get; private set; }
-    [Export] float cameraSpeed = 0.1f;
+    [Export] float cameraSpeed = 5f;
+    [Export] float zoomSpeed = 0.1f;
 
     public override void _Process(double delta)
     {
-        ProcessCameraMovement();
+        ProcessCameraMovement((float) delta);
         base._Process(delta);
     }
 
@@ -19,26 +20,26 @@ public partial class CameraController : Node3D
         base._Input(@event);
     }
 
-    private void ProcessCameraMovement()
+    private void ProcessCameraMovement(float delta)
     {
         // TODO: Tween?
 
         if(Input.IsActionPressed("Move Left"))
-            this.Position += cameraSpeed * Vector3.Left;
+            this.Position += cameraSpeed * Vector3.Left * delta;
         else if (Input.IsActionPressed("Move Right"))
-            this.Position += cameraSpeed * Vector3.Right;
+            this.Position += cameraSpeed * Vector3.Right * delta;
         
         if (Input.IsActionPressed("Move Forward"))
-            this.Position += cameraSpeed * Vector3.Forward;
+            this.Position += cameraSpeed * Vector3.Forward * delta;
         else if (Input.IsActionPressed("Move Backward"))
-            this.Position += cameraSpeed * Vector3.Back;
+            this.Position += cameraSpeed * Vector3.Back * delta;
     }
 
     private void TacticalCameraZoom(InputEvent @event)
     {
         if(@event.IsAction("Zoom In"))
-            Position += cameraSpeed * Vector3.Up;
+            Position += zoomSpeed * Vector3.Up;
         else if (@event.IsAction("Zoom Out"))
-            Position += cameraSpeed * Vector3.Down;
+            Position += zoomSpeed * Vector3.Down;
     }
 }
