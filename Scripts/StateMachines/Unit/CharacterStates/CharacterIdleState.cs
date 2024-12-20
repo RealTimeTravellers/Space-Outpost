d6830887f3903ad9ad2ac47f926b01a71c6d542e
Hakkology <yildizhakan88@gmail.com>
2024-12-20 21:59:54 +0300

    animation state fixes.

--- Scripts/StateMachines/Unit/CharacterStates/CharacterIdleState.cs
+++ Scripts/StateMachines/Unit/CharacterStates/CharacterIdleState.cs
@@ -4,30 +4,12 @@ public class CharacterIdleState : CharacterState
 {
     public override void Enter(Character character)
     {
-        GD.Print("Entering Idle State");
-    }
-
-    public override CharacterStateType Process(Character character)
-    {
-        return CheckState(character);
+        base.Enter(character);
+        character.CharacterController.GetStateMachine().RequestAnimation("idle");
     }
 
     public override CharacterStateType CheckState(Character character)
     {
-        if (Input.IsActionPressed("move"))
-            return CharacterStateType.Moving;
-            
-        if (Input.IsActionPressed("aim"))
-            return CharacterStateType.Aiming;
-            
-        if (Input.IsActionPressed("take_cover"))
-            return CharacterStateType.InCover;
-            
         return CharacterStateType.Idle;
-    }
-
-    public override void Exit(Character character)
-    {
-        GD.Print("Exiting Idle State");
     }
 }
--- Scripts/StateMachines/Unit/CharacterStates/CharacterMovingState.cs
+++ Scripts/StateMachines/Unit/CharacterStates/CharacterMovingState.cs
@@ -1,38 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Godot;
 
 public class CharacterMovingState : CharacterState
 {
     public override void Enter(Character character)
     {
-        GD.Print("Entering Moving State");
-    }
-
-    public override CharacterStateType Process(Character character)
-    {
-        return CheckState(character);
+        base.Enter(character);
+        character.CharacterController.GetStateMachine().RequestAnimation("move");
     }
 
     public override CharacterStateType CheckState(Character character)
     {
-        if (!Input.IsActionPressed("move"))
+        // NavAgent'in durumunu kontrol et
+        if (character.CharacterController._navAgent.IsNavigationFinished())
+        {
             return CharacterStateType.Idle;
-            
-        if (Input.IsActionPressed("aim"))
-            return CharacterStateType.Aiming;
-            
-        if (Input.IsActionPressed("take_cover"))
-            return CharacterStateType.InCover;
-            
+        }
+        
         return CharacterStateType.Moving;
     }
 
     public override void Exit(Character character)
     {
-        GD.Print("Exiting Moving State");
+        base.Exit(character);
+        character.CharacterController.GetStateMachine().RequestAnimation("idle");
     }
 }
